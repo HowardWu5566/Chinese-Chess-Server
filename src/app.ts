e529edf3426d6a7734fd6bbaeeec8f7dd1af19a4
HowardWu5566 <ggg1237890@gmail.com>
2024-08-02 16:35:28 +0800

    feat: create table

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ export type Board = [
 ]
 
 export interface Player {
+  id: string
   ws: WebSocket
   position: string
 }
@@ -42,24 +43,40 @@ export interface Table {
   spectators: string[]
   board: Board
   req: ('red' | 'black')[]
+  start: boolean
   turn: 'red' | 'black'
 }
 
 const PORT = 8080
 const players: Map<string, Player> = new Map<string, Player>()
 const tables: Map<string, Table> = new Map<string, Table>()
+const EMPTY_BOARD: Board = [
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+]
 
 const wss = new WebSocketServer({ port: PORT })
 
 wss.on('connection', ws => {
-  const playerId = generateId('player')
-  console.log(`${playerId} connects to server`)
-  const newPlayer: Player = { ws, position: 'lobby' }
-  players.set(playerId, newPlayer)
+  const newPlayerId = generateId('player')
+  console.log(`${newPlayerId} connects to server`)
+  const newPlayer: Player = { id: newPlayerId, ws, position: 'lobby' }
+  players.set(newPlayerId, newPlayer)
   const playerCount = players.size
   const tableCount = tables.size
   ws.send(
-    JSON.stringify({ type: 'Update Header', data: { playerId, tableCount } })
+    JSON.stringify({
+      type: 'Update Header',
+      data: { playerId: newPlayerId, tableCount }
+    })
   )
   ws.send(
     JSON.stringify({
@@ -82,25 +99,57 @@ wss.on('connection', ws => {
 
   ws.on('error', console.error)
 
-  ws.on('message', msgFromClient => {
-    console.log('received: %s', msgFromClient)
+  ws.on('message', (msg: string) => {
+    const msgFromClient = JSON.parse(msg)
+    const { type } = msgFromClient
+    if (type === 'Create Table') handleCreateTable(newPlayer)
   })
 
   ws.on('close', () => console.log('Someone leaves'))
 })
 
+function handleCreateTable(player: Player): void {
+  const newTableId: string = generateId('table')
+  const newTable: Table = {
+    id: newTableId,
+    red: player.id,
+    black: null,
+    spectators: [],
+    req: [],
+    start: false,
+    board: EMPTY_BOARD,
+    turn: 'red'
+  }
+  tables.set(newTableId, newTable)
+  player.position = newTableId
+
+  player.ws.send(
+    JSON.stringify({ type: 'Update Header', data: { tableId: newTableId } })
+  )
+  players.forEach(player =>
+    player.ws.send(
+      JSON.stringify({
+        type: 'Update Header',
+        data: { tableCount: tables.size }
+      })
+    )
+  )
+}
+
 export function generateId(prefix: string): string {
+  const checkingRange = prefix === 'player' ? players : tables
   let id: string
   do {
     id = prefix + (Math.floor(Math.random() * 9000) + 1000)
-  } while ((global as any).isIdDuplicate(id))
+  } while (isIdDuplicate(id, checkingRange))
   return id
 }
 
-export function isIdDuplicate(id: string): boolean {
-  const tables = (global as any).tables
-  const players = (global as any).players
-  return tables.has(id) || players.has(id)
+export function isIdDuplicate(
+  id: string,
+  checkingRange: Map<string, Player | Table>
+): boolean {
+  return checkingRange.has(id)
 }
 
 export default wss
